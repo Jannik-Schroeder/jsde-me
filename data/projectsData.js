b58647bc0a8a6e7db8e7061a2d0b18e316af6267
Jannik Schröder <jannik.schroeder05@gmail.com>
2023-08-17 12:25:27 +0200

    Added projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -10,6 +10,36 @@ const projectsData = [
     tech2: 'typescript',
     tech3: 'tailwindcss',
   },
+  {
+    title: 'SPWhub',
+    description: `SPWHub is a alliance of entrepreneurs, who are building a decentralized network.`,
+    imgSrc: '',
+    href: 'https://spwhub.com/',
+    github: 'https://github.com/spwhub',
+    tech1: 'descentralized',
+    tech2: 'kubernetes',
+    tech3: 'network',
+  },
+  {
+    title: 'Portfolio',
+    description: `This Website is a portfolio of my work. I built it using Next.js and Tailwind CSS.`,
+    imgSrc: '',
+    href: 'https://jsde.me',
+    github: 'https://github.com/jannik-schroeder/jsde-me',
+    tech1: 'nextjs',
+    tech2: 'tailwindcss',
+    tech3: 'docker',
+  },
+  {
+    title: 'HomeLab',
+    description: `This this is the Story af my HomeLab. Were i host all my Projects.`,
+    imgSrc: '',
+    href: 'https://scpx.de',
+    github: '',
+    tech1: 'linux',
+    tech2: 'computing',
+    tech3: 'network',
+  },
 ]
 
 export default projectsData
