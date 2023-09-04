a12b08bef3333aa1c16f8d8ea43ac9ee3a9fcace
Jannik <jannik.schroeder05@gmail.com>
2023-09-04 21:22:06 +0200

    Update quotesData.js

--- data/quotesData.js
+++ data/quotesData.js
@@ -3,4 +3,8 @@ export const quotes = [
     author: 'Andrew Tate',
     quote: `They want to get rich, but they have no plan to get rich! And a hope and a plan are two different things.`,
   },
+  {
+    author: 'Miyamoto Musashi',
+    quote: `It is better to be a warrior in a garden, than a gardener in a war.`,
+  },
 ]
