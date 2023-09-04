ab85a4f5c9726a61c3ba5eb6bebd6233b4651cab
Jannik <jannik.schroeder05@gmail.com>
2023-09-04 21:41:35 +0200

    Update quotesData.js

--- data/quotesData.js
+++ data/quotesData.js
@@ -7,4 +7,32 @@ export const quotes = [
     author: 'Miyamoto Musashi',
     quote: `It is better to be a warrior in a garden, than a gardener in a war.`,
   },
+  {
+    author: 'Keanu Reeves',
+    quote: `If you’re a lover, you gotta be a fighter. Because, if you don’t fight for your love, what kind of love do you have?`,
+  },
+  {
+    author: 'Dwayne Johnsen',
+    quote: `It's nice to be important but it's more important to be nice.`,
+  },
+  {
+    author: 'Albert Einstein',
+    quote: `The reason for time is so that everything doesn't happen at once`,
+  },
+  {
+    author: 'Kurt Cobain',
+    quote: `They laugh at me because I'm different. I laugh at them cause they're all the same.`,
+  },
+  {
+    author: 'David Goggins',
+    quote: `I don't stop when I'm tired. I stop when I'm done.`,
+  },
+  {
+    author: 'Plato',
+    quote: `As empty vessels make the loudest sound, so they that have the least wit are the greatest blabbers.`,
+  },
+  {
+    author: 'Andrew Tate',
+    quote: `It's pretty easy to win if you never lose.`,
+  },
 ]
